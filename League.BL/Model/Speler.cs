5b453e4a382a02623c0c0e53d8b2e120fdc27f96
Tom Vande Wiele <tvdewiel@hotmail.com>
2023-04-28 09:55:08 +0200

    Add project files.

---
+++ League.BL/Model/Speler.cs
@@ -0,0 +1,53 @@
+﻿using League.BL.Exceptions;
+
+namespace League.BL.Model
+{
+    public class Speler
+    {
+        internal Speler(string naam, int? lengte, int? gewicht)
+        {
+            ZetNaam(naam);
+            if (lengte.HasValue) ZetLengte((int)lengte);
+            if (gewicht!=null) ZetGewicht(gewicht.Value);
+        }
+        internal Speler(int id, string naam, int? lengte, int? gewicht) : this(naam,lengte,gewicht)
+        {
+            ZetId(id);
+        }
+        public int Id { get;private set; }
+        public string Naam { get;private set; }
+        public Team Team { get;private set; }
+        public int? Rugnummer { get;private set; }
+        public int? Lengte { get;private set; }
+        public int? Gewicht { get;private set; }
+        public void ZetTeam(Team team)
+        {
+
+        }
+        public void ZetNaam(string naam)
+        {
+            if (string.IsNullOrWhiteSpace(naam)) throw new SpelerException("ZetNaam");
+            Naam=naam.Trim();
+        }
+        public void ZetLengte(int lengte)
+        {
+            if (lengte < 150) throw new SpelerException("ZetLengte");
+            Lengte=lengte;
+        }
+        public void ZetGewicht(int gewicht)
+        {
+            if (gewicht < 50) throw new SpelerException("ZetGewicht");
+            Gewicht=gewicht;
+        }
+        public void ZetId(int id)
+        {
+            if (id <= 0) throw new SpelerException("ZetId");
+            Id=id;
+        }
+        public void ZetRugnummer(int rugnummer)
+        {
+            if ((rugnummer <= 0) || (rugnummer > 99)) throw new SpelerException("ZetRugnummer");
+            Rugnummer=rugnummer;
+        }
+    }
+}
---
+++ League.BL/Model/Team.cs
@@ -0,0 +1,49 @@
+﻿using League.BL.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace League.BL.Model
+{
+    public class Team
+    {
+        internal Team(int stamnummer, string naam)
+        {
+            ZetStamnummer(stamnummer);
+            ZetNaam(naam);
+        }
+        public int Stamnummer { get;private set; }
+        public string Naam { get; private set; }
+        public string Bijnaam { get; private set; }
+        private List<Speler> _spelers = new List<Speler>();
+        public IReadOnlyList<Speler> Spelers()
+        {
+            return _spelers.AsReadOnly();
+        }
+        public void VoegSpelerToe(Speler speler)
+        {
+
+        }
+        public void VerwijderSpeler(Speler speler)
+        {
+
+        }
+        public void ZetStamnummer(int stamnummer)
+        {
+            if (stamnummer <= 0) throw new TeamException("ZetStamnummer");
+            Stamnummer= stamnummer;
+        }
+        public void ZetNaam(string naam)
+        {
+            if (string.IsNullOrWhiteSpace(naam)) throw new TeamException("ZetNaam");
+            Naam= naam.Trim();
+        }
+        public void ZetBijnaam(string naam)
+        {
+            if (string.IsNullOrWhiteSpace(naam)) throw new TeamException("ZetBijnaam");
+            Bijnaam = naam.Trim();
+        }
+    }
+}
